25a226cf21637731e73ada928c052fc651c819a1
Dave Timberlake <dtfireball@hotmail.com>
2026-08-05 12:35:44 +1000

    Show new active jobs at top of list

--- app.js
+++ app.js
@@ -346,7 +346,7 @@ jobForm.addEventListener("submit", (event) => {
 
     pauseAnyRunningJob();
 
-    state.jobs.push({
+state.jobs.unshift({
       id: `${Date.now()}-${Math.random().toString(36).slice(2, 9)}`,
       ...jobData,
       createdAt: now,
